62672055a8bbd7cd429d8b14a3ded0bbe9a15a78
Sibin23 <sibinjack23@gmail.com>
2024-10-12 22:22:06 +0530

    did changes in pubspec.yaml

---
+++ lib/screens/sign_up_screen/sign_up_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ScreenSignUp extends StatelessWidget {
+  const ScreenSignUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(child: Center(child: Text('ScreenSignUp'),)),
+    );
+  }
+}
