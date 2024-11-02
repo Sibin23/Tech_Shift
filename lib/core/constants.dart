5b07c96c9fe59a831870a37a5f28b3872b899a17
Sibin23 <sibinjack23@gmail.com>
2024-11-02 22:35:37 +0530

    working on login and sign up ui

---
+++ lib/core/constants.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const h10 = SizedBox(height: 10);
+const h20 = SizedBox(height: 20);
+const h30 = SizedBox(height: 30);
+const h40 = SizedBox(height: 40);
+const h50 = SizedBox(height: 50);
+
+const w10 = SizedBox(width: 10);
+
+// Text Style
+
+final titleText = GoogleFonts.crimsonText(fontSize: 30);
+final categoryText = GoogleFonts.poppins(fontSize: 13);
+final subtitle = GoogleFonts.roboto(fontSize: 17);
+final paraText = GoogleFonts.roboto(fontSize: 15);
+
+// firebase userdata
+
+const userName = 'Name';
+const emailId = 'Email';
+const userPassword = 'Password';
+const tempPassword = 'tempPass';
+const userUid = 'uid';
+const userNumber = 'PhoneNumber';
